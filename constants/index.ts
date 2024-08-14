397a821971a1975c33c8708c989af10a79b61d14
Trong Doan <trong.doan@smoothstack.com>
2024-08-14 13:32:48 -0700

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+    {   
+        label: 'Home',
+        imgUrl: '/icons/Home.svg',
+        route: '/home'
+    },
+    {   
+        label: 'Upcoming',
+        imgUrl: '/icons/upcoming.svg',
+        route: '/upcoming'
+    },
+    {   
+        label: 'Previous',
+        imgUrl: '/icons/previous.svg',
+        route: '/previous'
+    },
+    {   
+        label: 'Recording',
+        imgUrl: '/icons/Video.svg',
+        route: '/recording'
+    },
+    {   
+        label: 'Personal Room',
+        imgUrl: '/icons/add-personal.svg',
+        route: '/personal-room'
+    },
+]
