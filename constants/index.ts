e40ab7069ab8945be7914d6f98497c8f087519b1
Trong Doan <trong.doan@smoothstack.com>
2024-08-15 06:37:16 -0700

    add nav link folder

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,5 @@
 export const sidebarLinks = [
+
     {   
         label: 'Home',
         imgUrl: '/icons/Home.svg',
@@ -24,4 +25,11 @@ export const sidebarLinks = [
         imgUrl: '/icons/add-personal.svg',
         route: '/personal-room'
     },
+]
+export const avatarImages = [
+    '/images/avatar-1.jpeg',
+    '/images/avatar-2.jpeg',
+    '/images/avatar-3.jpeg',
+    '/images/avatar-4.jpeg',
+    '/images/avatar-5.jpeg',
 ]
